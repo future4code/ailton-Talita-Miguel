51970f9f55cf9550b7b17ad02d4c846d3bf0c398
Talita Miguel <talita.leonardo@gmail.com>
2022-05-24 13:27:40 -0300

    exercicio revisão - 12

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -205,7 +205,10 @@ function retornaChamadaDeFilme(filme) {
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-
+    return {
+        ...pessoa,
+        nome: "ANÔNIMO"
+    }
 }
 
 // EXERCÍCIO 13A
