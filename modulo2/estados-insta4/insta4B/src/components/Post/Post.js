7b968962ceabdaa3e37e640dae70cf1fa7fe17fe
Talita Miguel <talita.leonardo@gmail.com>
2022-06-09 17:09:42 -0300

    Atividade - Aula - Renderização de Listas

---
+++ modulo2/estados-insta4/insta4B/src/components/Post/Post.js
@@ -0,0 +1,168 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import {IconeComContador} from '../IconeComContador/IconeComContador'
+import {IconeSalvar} from '../IconeSalvar/IconeSalvar'
+import { IconeEnviar } from '../IconeEnviar/IconeEnviar'
+
+import iconeCoracaoBranco from '../../img/favorite-white.svg'
+import iconeCoracaoPreto from '../../img/favorite.svg'
+import iconeSalvarPreto from '../../img/save.svg'
+import iconeSalvarBranco from '../../img/save-white.svg'
+import iconeEnviar from '../../img/send.svg'
+import iconeComentario from '../../img/comment_icon.svg'
+
+import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
+import { SecaoCompartilhar } from '../SecaoCompartilhar/SecaoCompartilhar'
+
+const PostContainer = styled.div`
+  border: 1px solid gray;
+  width: 300px;
+  margin-bottom: 10px;
+`
+
+const PostHeader = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+`
+
+const PostFooter = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  justify-content: space-between;
+`
+
+const UserPhoto = styled.img`
+  height: 30px;
+  width: 30px;
+  margin-right: 10px;
+  border-radius: 50%;
+`
+
+const PostPhoto = styled.img`
+  width: 100%;
+`
+
+class Post extends React.Component {
+  state = {
+    curtido: false,
+    salvar: false,
+    enviando: false, 
+    numeroCurtidas: 0,
+    comentando: false,
+    numeroComentarios: 0
+  }
+
+  onClickCurtida = () => {
+    this.setState({
+      curtido: !this.state.curtido
+    })
+
+    if(this.state.curtido) {
+      this.setState({
+        numeroCurtidas: this.state.numeroCurtidas - 1
+      })
+
+    } else {
+      this.setState({
+        numeroCurtidas: this.state.numeroCurtidas + 1
+      })
+    }
+  }
+
+  onClickComentario = () => {
+    this.setState({
+      comentando: !this.state.comentando
+    })
+  }
+
+  onClickSalvar = () => {
+    this.setState({
+      salvar: !this.state.salvar
+    })
+  }
+
+  onClickEnviar = () => {
+    this.setState({
+      enviando: !this.state.enviando
+    })
+  }
+
+  aoEnviarComentario = () => {
+    this.setState({
+      comentando: false,
+      numeroComentarios: this.state.numeroComentarios + 1
+    })
+  }
+
+  render() {
+    let iconeCurtida
+
+    if(this.state.curtido) {
+      iconeCurtida = iconeCoracaoPreto
+    } else {
+      iconeCurtida = iconeCoracaoBranco
+    }
+
+    let iconeSalvar
+
+    if(this.state.salvar) {
+      iconeSalvar = iconeSalvarPreto
+    } else {
+      iconeSalvar = iconeSalvarBranco
+    }
+
+    let componenteComentario
+
+    if(this.state.comentando) {
+      componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario}/>
+    }
+
+    let componenteCompartilhar
+
+    if(this.state.enviando) {
+      componenteCompartilhar = <SecaoCompartilhar />
+    }
+
+    return <PostContainer>
+      <PostHeader>
+        <UserPhoto src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
+        <p>{this.props.nomeUsuario}</p>
+      </PostHeader>
+
+      <PostPhoto src={this.props.fotoPost} alt={'Imagem do post'}/>
+
+      <PostFooter>
+        <IconeComContador
+          icone={iconeCurtida}
+          onClickIcone={this.onClickCurtida}
+          valorContador={this.state.numeroCurtidas}
+        />
+
+        <IconeSalvar 
+          icone={iconeSalvar}
+          onClickIcone={this.onClickSalvar}
+        />
+
+        <IconeEnviar 
+          icone={iconeEnviar}
+          onClickIcone={this.onClickEnviar}
+        />
+
+        <IconeComContador
+          icone={iconeComentario}
+          onClickIcone={this.onClickComentario}
+          valorContador={this.state.numeroComentarios}
+        />
+      </PostFooter>
+      {componenteComentario}
+      {componenteCompartilhar}
+    </PostContainer>
+  }
+}
+
+export default Post
